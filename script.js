e17e7853f62be842b02ddddbb264974af1fee0c6
SabrinaGir <116656240+SabrinaGir@users.noreply.github.com>
2023-12-16 10:49:08 -0500

    Set up files and basic calculations in console

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let firstNum = 5;
+let secondNum = 3;
+let operator = '*';
+
+add(5,3);
+
+
+function add(a,b) {
+    console.log(a + b)
+}
+
+function subtract(a,b) {
+    console.log(a - b)
+}
+
+function multiply(a,b) {
+    console.log(a * b)
+}
+
+function divide(a,b) {
+    console.log(a / b)
+}
