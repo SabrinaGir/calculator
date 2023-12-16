d9bddd30679746a288146bc591c35501a2e0c4e7
SabrinaGir <116656240+SabrinaGir@users.noreply.github.com>
2023-12-16 14:45:20 -0500

    Get calculator working

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict'
 
 // find all buttons
 
@@ -5,43 +6,71 @@ let buttons = document.querySelectorAll('button');
 
 let screen = document.querySelector('#screen');
 
-// let one = document.querySelector('#one');
-// let two = document.querySelector('#two');
-// let three = document.querySelector('#three');
-
-// let addButton = document.querySelector('#add');
-
-// let equalButton = document.querySelector('#equal');
+let equalButton = document.querySelector('#equal');
 
 
 
 // let the user input numbers
 
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = ''
+let secondNum = ''
+let operator = ''
+let answer = ''
+
 
 buttons.forEach(button => {
     button.addEventListener('click', event => {
-        screen.textContent = screen.textContent + ' ' + button.textContent;
+        screen.textContent = updateScreen(button);
     })
+})
+
+equalButton.addEventListener('click', event => {
+    operate(firstNum, secondNum, operator);
 })
 
 
 
+
+function updateScreen(input) {
+    if (answer != '') {
+        firstNum = ''
+        secondNum = ''
+        operator = ''
+        answer = ''
+    }
+
+
+    if (input.className == 'num' && operator == '') {
+        firstNum = firstNum + input.textContent;
+    }
+    else if (input.className == 'op' && firstNum != '') {
+        operator = input.textContent;
+    }
+    else if (input.className == 'num' && operator != '') {
+        secondNum = secondNum + input.textContent;
+    }
+
+    return firstNum + ' ' + operator + ' ' + secondNum;
+}
+
 // called after all three variables are assigned and performs operation according to the operator chosen
 function operate(a,b, operator) {
-    if (operator == '+') {
-        add(a,b);
-    }
-    else if (operator == '-') {
-        subtract(a,b)
-    }
-    else if (operator == '*') {
-        multiply(a,b)
-    }
-    else if (operator == '/') {
-        divide(a,b)
+    if (firstNum != '' && operator != '' && secondNum != '') {
+        // change them into numbers so that calculations can be performed
+        a = Number(a);
+        b = Number(b);
+        if (operator == '+') {
+            add(a,b);
+        }
+        else if (operator == '-') {
+            subtract(a,b)
+        }
+        else if (operator == '*') {
+            multiply(a,b)
+        }
+        else if (operator == '/') {
+            divide(a,b)
+        }
     }
 
 }
@@ -49,18 +78,22 @@ function operate(a,b, operator) {
 
 // functions used to add, subtract, multiply and divide firstNum and secondNum
 function add(a,b) {
-    console.log(a + b)
+    answer = a + b
+    screen.textContent = answer
 }
 
 function subtract(a,b) {
-    console.log(a - b)
+    answer = a - b
+    screen.textContent = answer
 }
 
 function multiply(a,b) {
-    console.log(a * b)
+    answer = a * b
+    screen.textContent = answer
 }
 
 function divide(a,b) {
-    console.log(a / b)
+    answer = a / b
+    screen.textContent = answer
 }
 
