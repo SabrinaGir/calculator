94bb9e9c28f5aee7ff28883ffd436ca3159f50f0
SabrinaGir <116656240+SabrinaGir@users.noreply.github.com>
2023-12-16 11:24:36 -0500

    Make calculator in HTML

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 
-let firstNum = 5;
+// find all buttons
+
+let one = document.querySelector('#one');
+let two = document.querySelector('#two');
+let three = document.querySelector('#three');
+
+let addButton = document.querySelector('#add');
+
+let equalButton = document.querySelector('#equal');
+
+
+
+// let the user input numbers
+
+let firstNum = ;
 let secondNum = 3;
-let operator = '*';
-
-add(5,3);
+let operator = '-';
 
 
+operate(firstNum, secondNum, operator);
+
+
+
+
+// called after all three variables are assigned and performs operation according to the operator chosen
+function operate(a,b, operator) {
+    if (operator == '+') {
+        add(a,b);
+    }
+    else if (operator == '-') {
+        subtract(a,b)
+    }
+    else if (operator == '*') {
+        multiply(a,b)
+    }
+    else if (operator == '/') {
+        divide(a,b)
+    }
+
+}
+
+
+// functions used to add, subtract, multiply and divide firstNum and secondNum
 function add(a,b) {
     console.log(a + b)
 }
@@ -21,3 +57,4 @@ function multiply(a,b) {
 function divide(a,b) {
     console.log(a / b)
 }
+
